41449664c90626db3bb76b102d4ce4e234b967b6
DevRys <eduardoreyes55@aragon.unam.mx>
2024-03-03 17:52:04 -0600

    Disbale map page

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,31 +1,38 @@
-"use client";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+// "use client";
+// import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
 
-if (typeof window !== "undefined") {
-  require("leaflet-defaulticon-compatibility");
-  require("leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css");
+// if (typeof window !== "undefined") {
+//   require("leaflet-defaulticon-compatibility");
+//   require("leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css");
+// }
+
+// const Map = () => {
+//   return (
+//     <MapContainer
+//       center={[51.505, -0.09]}
+//       zoom={13}
+//       scrollWheelZoom={false}
+//       style={{ height: 400, width: "100%" }}
+//     >
+//       <TileLayer
+//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={[51.505, -0.09]}>
+//         <Popup>
+//           A pretty CSS3 popup. <br /> Easily customizable.
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
+//   );
+// };
+
+// export default Map;
+import React from "react";
+
+function page() {
+  return <div>page</div>;
 }
 
-const Map = () => {
-  return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: 400, width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
-};
-
-export default Map;
+export default page;
