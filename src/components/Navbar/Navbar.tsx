0c36989bdec99fde37dcd30c95f53acae88b54da
DevRys <lalobaker3@gmail.com>
2024-10-07 22:26:31 -0600

    feat: optimize performance by adding useCallback to functions

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React from "react";
+
+import React, { useState, useCallback } from "react";
 import { privateRoutes, publicRoutes } from "./NavbarRoutes";
 import { AcmeLogo } from "./assets/AcmeLogo";
 import {
@@ -23,13 +24,22 @@ import { useTheme } from "next-themes";
 import { DarkModeIcon, LightModeIcon } from "../icons/icons";
 
 export default function NavbarUI() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const { theme, setTheme, systemTheme } = useTheme();
-
-  // const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { theme, setTheme } = useTheme();
   const pathname = usePathname();
-  const isCurrentRoute = (routePath: string) => pathname === routePath;
+
+  const isCurrentRoute = useCallback(
+    (routePath: string) => pathname === routePath,
+    [pathname]
+  );
+
+  const handleThemeToggle = useCallback(() => {
+    setTheme(theme === "light" ? "dark" : "light");
+  }, [theme, setTheme]);
+
+  const handleMenuToggle = useCallback(() => {
+    setIsMenuOpen((prev) => !prev);
+  }, []);
 
   return (
     <Navbar isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
@@ -45,26 +55,20 @@ export default function NavbarUI() {
           </Link>
         </NavbarBrand>
       </NavbarContent>
+      
       <NavbarContent justify="end">
         <NavbarItem>
-          {theme == "light" ? (
+          {theme === "light" ? (
             <DarkModeIcon
               className="cursor-pointer text-xl"
-              onClick={() => {
-                setTheme("dark");
-              }}
-            ></DarkModeIcon>
+              onClick={handleThemeToggle}
+            />
           ) : (
             <LightModeIcon
               className="cursor-pointer text-xl text-yellow-300"
-              onClick={() => {
-                setTheme("light");
-              }}
-            ></LightModeIcon>
+              onClick={handleThemeToggle}
+            />
           )}
-          {/* <Button color="primary" variant="bordered" onClick={handleSignOut}>
-                  Log Out
-                </Button> */}
         </NavbarItem>
       </NavbarContent>
 
@@ -80,15 +84,11 @@ export default function NavbarUI() {
           </NavbarItem>
         ))}
       </NavbarContent>
+
       <NavbarMenu>
         {publicRoutes.map((route) => (
           <NavbarMenuItem key={route.id}>
-            <Link
-              href={route.path}
-              onClick={() => {
-                setIsMenuOpen(!isMenuOpen);
-              }}
-            >
+            <Link href={route.path} onClick={handleMenuToggle}>
               {route.name}
             </Link>
           </NavbarMenuItem>
